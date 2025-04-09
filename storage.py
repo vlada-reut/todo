1a265ab6b1c95d2c1e34a1c14343ed384c61c35e
Vlada Reut <vladareut@mail.ru>
2025-04-10 01:02:01 +0300

    fixed an error when renaming TASKS_DB

--- storage.py
+++ storage.py
@@ -7,10 +7,10 @@ TASKS_DB = "tasks.json"  # Файл для хранения задач
 
 def load_tasks():
     """Загружает задачи из файла, если он существует."""
-    if not os.path.exists(TASKS_FILE):
+    if not os.path.exists(TASKS_DB):
         return []
     try:
-        with open(TASKS_FILE, "r", encoding="utf-8") as file:
+        with open(TASKS_DB, "r", encoding="utf-8") as file:
             data = json.load(file)
             return data if isinstance(data, list) else []
     except json.JSONDecodeError:
@@ -19,6 +19,6 @@ def load_tasks():
 
 def save_tasks(task_list):
     """Сохраняет задачи в файл."""
-    with open(TASKS_FILE, "w", encoding="utf-8") as file:
+    with open(TASKS_DB, "w", encoding="utf-8") as file:
         json.dump([t.to_dict() for t in task_list], file, indent=4, ensure_ascii=False)
     print("💾 Задачи успешно сохранены.")
